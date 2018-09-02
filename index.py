b27eefe5cd3e3be4c2b72f041678878095ee8aa5
Irina <38900844+LLevella@users.noreply.github.com>
2018-09-02 13:11:46 +0700

    исправлена повтоорная отравка формы

--- index.py
+++ index.py
@@ -5,8 +5,10 @@ import cgi
 import http.cookies
 import os
 
+
 from main_functions import SignUserData
 from main_functions import CookieHandler
+from main_functions import PostHandler
 
 from html_templates import signin_tmp
 from html_templates import signin_err_tmp
@@ -20,40 +22,69 @@ def get_random():
 
 if __name__ == "__main__":
 
+    name_user_key = "user_key"
     form = cgi.FieldStorage()
-    action = form.getfirst("action", "")
+    user_key = form.getfirst(name_user_key, "")
+    ph = PostHandler(name_user_key)
 
     ch = CookieHandler()
     cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
     hash_key_value = cookie.get("key")
+    
     if hash_key_value is not None:
         hash_key_value = hash_key_value.value
-    
-    if action == "":
-        user = ch.find_cookie(hash_key_value)
-        if user is None:
+
+    if user_key == "":
+    # если post - запрос не сериализован и ключ пользователю не назначен
+        action = form.getfirst("action", "")
+        if action:
+        # если post - запрос был отправлен
+            ph.get_post_data(form)
+            ph.save_post_data()
+            ph.redirect_to_get_server()
+        else:
+        # если активности еще не было
+            user_by_cookie = ch.find_cookie(hash_key_value)
+            if user_by_cookie is None:
             #если куки не установлены или пользователь не найден - страница входа
-            print_function(signin_tmp) 
-        else:
+                print_function(signin_tmp) 
+            else:
             #куки стоят и такой пользователь есть - основная
-            print_function(signin_ok_tmp, str(get_random()))
+                print_function(signin_ok_tmp, str(get_random()))
+    else:
+    # если уже post - запрос сериализован и выдан ключ пользователю
+        if ph.find_post_data(user_key):
+        # если ключ валидный
+            action = ph.get_value("action", "")
+            if action == "":
+                user = ch.find_cookie(hash_key_value)
+                if user is None:
+                #если куки не установлены или пользователь не найден - страница входа
+                    print_function(signin_tmp) 
+                else:
+                #куки стоят и такой пользователь есть - основная
+                    print_function(signin_ok_tmp, str(get_random()))
 
-    if action == "exit":
-        #удаляем куки при выходе
-        key = ch.delete_cookie(hash_key_value)
-        print('Set-cookie: key={}'.format(key))
-        print_function(signin_tmp)
-
-    if action == "login":
-        sud = SignUserData(form)
-        if sud.is_login_ok():
-            if sud.is_remember_me_checked():
-                key = ch.set_cookie(sud.get_login(),sud.get_password)
+            if action == "exit":
+            #удаляем куки при выходе
+                key = ch.delete_cookie(hash_key_value)
                 print('Set-cookie: key={}'.format(key))
-            print_function(signin_ok_tmp, str(get_random()))
+                print_function(signin_tmp)
+
+            if action == "login":
+            # логинимся 
+                login = ph.get_value("login")
+                password = ph.get_value("password")
+                remember_me = ph.get_value("remember_me")
+                sud = SignUserData(login, password, remember_me)
+                if sud.is_login_ok():
+                    if sud.is_remember_me_checked():
+                        key = ch.set_cookie(sud.get_login(),sud.get_password)
+                        print('Set-cookie: key={}'.format(key))
+                    print_function(signin_ok_tmp, str(get_random()))
+                else:
+                    print_function(signin_err_tmp)
+            ph.delete_post_data(user_key)
         else:
-            print_function(signin_err_tmp)
-
-
-
-
+        # если ключ не найден
+            print_function(signin_tmp)
--- main_functions.py
+++ main_functions.py
@@ -1,6 +1,10 @@
 import cgi
 import html
 import json
+import os
+import sys
+import urllib
+import random
 
 def files_init(file_name):
     try:
@@ -14,11 +18,10 @@ class SignUserData:
     
     USERS = 'tmp/users.json'
 
-    def __init__(self, form):
-        self.form = form
-        self.login =  html.escape(form.getfirst('login',''))
-        self.password = html.escape(form.getfirst('password',''))
-        self.remember_me = self.form.getvalue('remember_me')
+    def __init__(self, login, password, remember_me):
+        self.login = login
+        self.password = password
+        self.remember_me = remember_me
         files_init(self.USERS)
 
     def is_remember_me_checked(self):
@@ -30,7 +33,7 @@ class SignUserData:
         if self.login in users and self.password == users[self.login]: 
             return True     
         return False
-    
+        
     def get_login(self):
         return self.login
     
@@ -69,3 +72,66 @@ class CookieHandler:
         with open(self.COOKIE, 'w', encoding='utf-8') as f:
             json.dump(cookies, f)
         return 0
+
+class PostHandler:
+
+    GET_SERVER = 'http://localhost/index.py'
+    POST = 'tmp/post.json'
+
+    def __init__(self, name_user_key):
+        self.name_user_key = name_user_key
+        files_init(self.POST)
+    
+    def get_post_data(self, form): 
+        self.post_data = {}
+        str_post_hash = ""
+        self.post_data['action'] =html.escape(form.getfirst("action", ""))
+        self.post_data['login'] = html.escape(form.getfirst("login", ""))
+        self.post_data['password'] = html.escape(form.getfirst("password", ""))
+        self.post_data['remember_me'] = html.escape(form.getfirst("remember_me", ""))
+        for post_arg in self.post_data: 
+            str_post_hash += str(post_arg)
+        str_post_hash += str(random.random())
+        self.post_hash = hash(str_post_hash)
+        # print(self.post_data)
+
+    def create_straddres_for_redirect(self):
+        post_key = {self.name_user_key: self.post_hash}
+        return '?'.join((self.GET_SERVER, urllib.parse.urlencode(post_key)))
+
+    def redirect_to_get_server(self):
+        print ("Content-Type: text/html")
+        print ("Location: {}".format(self.create_straddres_for_redirect()))
+        print ("")
+    
+    def save_post_data(self):
+        with open(self.POST, 'r', encoding='utf-8') as f:
+            post_tmp = json.load(f)
+        post_tmp[self.post_hash] = self.post_data
+        # print(post_tmp)
+        with open(self.POST, 'w', encoding='utf-8') as f:
+            json.dump(post_tmp, f)
+    
+    def find_post_data(self, post_key):
+        with open(self.POST, 'r', encoding='utf-8') as f:
+            post_tmp = json.load(f)
+            if post_tmp.get(post_key) is None: 
+                return False
+            post_data_tmp = post_tmp.get(post_key)
+            if not post_data_tmp:
+                return False
+            self.post_data = post_data_tmp
+        return True
+    
+    def get_value(self, name, default = ""):
+        if self.post_data[name] is None:
+            return default
+        return self.post_data[name]
+    
+    def delete_post_data(self, post_key):
+        with open(self.POST, 'r', encoding='utf-8') as f:
+            post_tmp = json.load(f)
+        if post_tmp.get(post_key) is not None:
+            post_tmp.pop(post_key)
+        with open(self.POST, 'w', encoding='utf-8') as f:
+            json.dump(post_tmp, f)
